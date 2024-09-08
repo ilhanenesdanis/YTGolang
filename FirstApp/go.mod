ff95e89c34dc5706d7fa129b4d4fa36f5481de57
ilhan enes daniş <danisenes3@gmail.com>
2024-09-09 00:27:52 +0300

    go mod

---
+++ FirstApp/go.mod
@@ -0,0 +1,5 @@
+module ytGolang
+
+go 1.21.6
+
+require github.com/twpayne/go-geom v1.5.7 // indirect
--- FirstApp/main.go
+++ FirstApp/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"github.com/twpayne/go-geom"
+)
 
 func main() {
-	fmt.
+	fmt.Println("Hello GO")
+
+	println("Hello GO")
+
+	// geom.NewMultiPolygon()
 }
+
+//modül oluşturmak için go mod init komutunu çalıştırıyoruz
+//projeye ek bir modül eklemek için go get github.com/twpayne/go-geom gibi bir komut çalıştırmak gerekli
+//go kullanılmayan paketleri import içinde tutmaz
+//go mod tidy kullanılmayan depencyleri kaldırır henüz yüklenmemiş olanlarıda indirir
