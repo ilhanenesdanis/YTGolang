4281af46ee620dc3911566c10055cd62301310bd
ilhan enes daniş <danisenes3@gmail.com>
2024-09-26 08:56:55 +0300

    interfaces 2

--- interfaces/main.go
+++ interfaces/main.go
@@ -21,10 +21,21 @@ func main() {
 
 	fmt.Println(electronic1.ShippingCost())
 
-}
+	electronics := []Electronic{{Desi: 10}, {Desi: 20}}
+	calculateTotalShippingCostOfElectronics(electronics)
 
-func (book *Book) ShippingCost() int {
-	return 5 + book.Desi*2
+	fmt.Println("İnterfaces")
+
+	var product IShippable
+
+	product = &Book{Desi: 10}
+
+	fmt.Println(product.ShippingCost())
+
+	product = &Electronic{Desi: 12}
+
+	fmt.Println(product.ShippingCost())
+
 }
 
 func calculateTotalShippingCost(books []Book) int {
@@ -35,10 +46,21 @@ func calculateTotalShippingCost(books []Book) int {
 	return total
 
 }
+func calculateTotalShippingCostOfElectronics(electronics []Electronic) int {
+	total := 0
+	for _, electronic := range electronics {
+		total += electronic.ShippingCost()
+	}
+	return total
+
+}
 
 func (electronic *Electronic) ShippingCost() int {
 	return 10 + electronic.Desi*3
 
+}
+func (book *Book) ShippingCost() int {
+	return 5 + book.Desi*2
 }
 
 type Book struct {
@@ -46,4 +68,7 @@ type Book struct {
 }
 type Electronic struct {
 	Desi int
+}
+type IShippable interface {
+	ShippingCost() int
 }
