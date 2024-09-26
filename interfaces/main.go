c23941062f9cfa3e8c7d58f7a65b83ef474abce4
ilhan enes daniş <danisenes3@gmail.com>
2024-09-26 09:04:11 +0300

    interface 3

--- interfaces/main.go
+++ interfaces/main.go
@@ -4,69 +4,44 @@ import "fmt"
 
 func main() {
 
-	book := &Book{Desi: 25}
-
-	cost := book.ShippingCost()
-
-	fmt.Println(cost)
-
-	books := []Book{
-		{Desi: 50},
-		{Desi: 20},
-		{Desi: 10},
+	var products []IShippable = []IShippable{
+		&Book{Desi: 10},
+		&Book{Desi: 20},
+		&Electronic{Desi: 20},
+		&Flover{Desi: 3},
 	}
-	fmt.Println(calculateTotalShippingCost(books))
 
-	electronic1 := &Electronic{Desi: 20}
-
-	fmt.Println(electronic1.ShippingCost())
-
-	electronics := []Electronic{{Desi: 10}, {Desi: 20}}
-	calculateTotalShippingCostOfElectronics(electronics)
-
-	fmt.Println("İnterfaces")
-
-	var product IShippable
-
-	product = &Book{Desi: 10}
-
-	fmt.Println(product.ShippingCost())
-
-	product = &Electronic{Desi: 12}
-
-	fmt.Println(product.ShippingCost())
+	fmt.Println(calculateTotalShippingCost(products))
 
 }
 
-func calculateTotalShippingCost(books []Book) int {
+func calculateTotalShippingCost(products []IShippable) int {
 	total := 0
-	for _, book := range books {
-		total += book.ShippingCost()
-	}
-	return total
-
-}
-func calculateTotalShippingCostOfElectronics(electronics []Electronic) int {
-	total := 0
-	for _, electronic := range electronics {
-		total += electronic.ShippingCost()
+	for _, product := range products {
+		total += product.ShippingCost()
 	}
 	return total
 
 }
 
 func (electronic *Electronic) ShippingCost() int {
-	return 10 + electronic.Desi*3
+	return 5 + electronic.Desi*3
 
 }
 func (book *Book) ShippingCost() int {
-	return 5 + book.Desi*2
+	return 5 + book.Desi*3
+}
+func (flover *Flover) ShippingCost() int {
+	return 5 + flover.Desi*3
 }
 
 type Book struct {
 	Desi int
 }
 type Electronic struct {
+	Desi int
+}
+type Flover struct {
 	Desi int
 }
 type IShippable interface {
